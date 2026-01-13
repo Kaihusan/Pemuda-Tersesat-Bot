0936b07d89c2da96c8ecc0b816e0ee1d14099fb9
Kaihu <kaihu2790@gmail.com>
2026-01-13 10:04:10 +0700

    Update kode: slash commands index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const fs = require("fs");
 
 const client = new Client({
